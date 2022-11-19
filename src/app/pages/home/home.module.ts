c4be413bc91e2f644d2d316f86ba06d1d2f94734
bacini <bagasp5621@gmail.com>
2022-11-19 10:04:09 +0700

    add home component

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,3 +1,4 @@
+import { HomePageComponent } from '../../components/home-page/home-page.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -9,12 +10,7 @@ import { HomePageRoutingModule } from './home-routing.module';
 import { HomePage } from './home.page';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    HomePageRoutingModule
-  ],
-  declarations: [HomePage]
+  imports: [CommonModule, FormsModule, IonicModule, HomePageRoutingModule],
+  declarations: [HomePage, HomePageComponent],
 })
 export class HomePageModule {}

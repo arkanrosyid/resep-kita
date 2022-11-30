09c3fb8fdec15fbca4b1169cc54f656499c13dbb
arkanrosyid <ahmadrosyid53@gmail.com>
2022-11-30 23:55:23 +0700

    belum fix

--- src/app/pages/resep-saya/resep-saya.page.ts
+++ src/app/pages/resep-saya/resep-saya.page.ts
@@ -1,7 +1,8 @@
 import { Router } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { getElement } from 'ionicons/dist/types/stencil-public-runtime';
 
 @Component({
   selector: 'app-resep-saya',
@@ -9,7 +10,10 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
   styleUrls: ['./resep-saya.page.scss'],
 })
 export class ResepSayaPage implements OnInit {
+  // resepSaya= "";
+  @ViewChild('myList') myList:ElementRef;
   constructor(
+    private renderer: Renderer2,
     private router: Router,
     private afs: AngularFirestore,
     public angularFireAuth: AngularFireAuth
@@ -34,6 +38,30 @@ export class ResepSayaPage implements OnInit {
     const resep = await this.afs
       .collection('Resep', (ref) => ref.where('user', '==', email))
       .valueChanges()
-      .subscribe((data) => console.log(data.toString()));
+      .subscribe((data) =>  { 
+      //  var resepSaya = document.getElementById('resepSaya');
+       let html = "";
+        data.forEach(function (item ,key) {
+          
+          html+= '<ion-item lines="none" button (click)="resep()">\
+          <ion-thumbnail slot="start">\
+            <img\
+              alt="'+item['judul']+'"\
+              src="'+item['gambar']+'"\
+            />\
+          </ion-thumbnail>\
+          <ion-label>'+item['judul']+'</ion-label>\
+        </ion-item>';
+        console.log(html);
+        })
+        
+      });
+      
+      
+      
+  }
+
+  objectValues(obj){
+    return Object.keys(obj);
   }
 }

a9316fb6cd022ceb4337ec0222163e9284dd25ea
arkanrosyid <ahmadrosyid53@gmail.com>
2022-11-30 21:53:40 +0700

    a

--- src/app/pages/tulis-resep/tulis-resep.page.ts
+++ src/app/pages/tulis-resep/tulis-resep.page.ts
@@ -14,4 +14,38 @@ export class TulisResepPage implements OnInit {
   home() {
     this.router.navigate(['home']);
   }
+<<<<<<< Updated upstream
 }
+=======
+  createResep() {
+    const email = this.angularFireAuth.currentUser.then((data) => data.email);
+
+    const resep = {
+      gambar: this.obj.photoUrl,
+      judul: this.judul,
+      bahan: this.bahan,
+      langkah: this.langkah,
+      jenis: this.jenis,
+      user: email,
+    };
+
+    this.home();
+
+    const resepJSON = JSON.parse(JSON.stringify(resep));
+    return this.afs.collection('/Resep').add(resepJSON);
+  }
+
+  onFileSelect(input) {
+    console.log(input.files);
+    if (input.files && input.files[0]) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        console.log(e.target.result);
+        this.obj.photoUrl = e.target.result;
+      };
+      reader.readAsDataURL(input.files[0]);
+      // 
+    }
+  }
+}
+>>>>>>> Stashed changes

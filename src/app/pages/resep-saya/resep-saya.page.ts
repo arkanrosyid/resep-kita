ba4caa5905b802041819d562a99cd65e841dc146
bacini <bagasp5621@gmail.com>
2022-12-01 01:58:41 +0700

    read work

--- src/app/pages/resep-saya/resep-saya.page.ts
+++ src/app/pages/resep-saya/resep-saya.page.ts
@@ -1,8 +1,8 @@
+/* eslint-disable @typescript-eslint/dot-notation */
 import { Router } from '@angular/router';
-import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { getElement } from 'ionicons/dist/types/stencil-public-runtime';
 
 @Component({
   selector: 'app-resep-saya',
@@ -10,10 +10,9 @@ import { getElement } from 'ionicons/dist/types/stencil-public-runtime';
   styleUrls: ['./resep-saya.page.scss'],
 })
 export class ResepSayaPage implements OnInit {
-  // resepSaya= "";
-  @ViewChild('myList') myList:ElementRef;
+  reseps: any;
+
   constructor(
-    private renderer: Renderer2,
     private router: Router,
     private afs: AngularFirestore,
     public angularFireAuth: AngularFireAuth
@@ -35,33 +34,15 @@ export class ResepSayaPage implements OnInit {
     const email = await this.angularFireAuth.currentUser.then(
       (data) => data.email
     );
-    const resep = await this.afs
+    this.afs
       .collection('Resep', (ref) => ref.where('user', '==', email))
-      .valueChanges()
-      .subscribe((data) =>  { 
-      //  var resepSaya = document.getElementById('resepSaya');
-       let html = "";
-        data.forEach(function (item ,key) {
-          
-          html+= '<ion-item lines="none" button (click)="resep()">\
-          <ion-thumbnail slot="start">\
-            <img\
-              alt="'+item['judul']+'"\
-              src="'+item['gambar']+'"\
-            />\
-          </ion-thumbnail>\
-          <ion-label>'+item['judul']+'</ion-label>\
-        </ion-item>';
-        console.log(html);
-        })
-        
-      });
-      
-      
-      
-  }
-
-  objectValues(obj){
-    return Object.keys(obj);
+      .snapshotChanges()
+      .subscribe(
+        (data) =>
+          (this.reseps = data.map((e) => ({
+            judul: e.payload.doc.data()['judul'],
+            gambar: e.payload.doc.data()['gambar'],
+          })))
+      );
   }
 }
